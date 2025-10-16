e5a1bb782693f279cbbe9b1352f67ca74ab847e1
Francisgomesx <fpsgomes9@gmail.com>
2025-10-16 19:29:29 +0100

    New file intro.sh with the intro for the game and game.sh cleaned up. It will function as the maing file with all of the other ones connected into it so that the game runs in it.

---
+++ intro.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+trap 'echo -e "\n[Underlink terminated]"; stty echo; exit 0' SIGINT
+
+stty -echo
+
+echo
+echo
+echo "====================================="
+echo
+echo "      Welcome to the Underlink"
+echo
+echo "====================================="
+echo
+echo
+
+sleep 2.5
+
+echo "You weren't cautious and now you are stuck."
+echo
+
+sleep 2.5
+
+echo "You are now linked. There is no exit!"
+echo
+
+sleep 2.5
+
+echo "Watch out for the dangers while crossing the Link."
+echo
+
+sleep 2.5
+
+echo "Good Luck!"
+echo
+
+sleep 2.5
+
+echo "Start game? (Y/N)?"
+stty echo
+
+read -n1 answer
+stty -echo
+echo
+
+if [[ $answer == "y" || $answer == "Y" ]]; then
+
+  echo
+  echo "Making connection to link..."
+  echo
+  sleep 2.5
+
+else
+
+  echo
+  echo "I said you can't escape! Making connection to link..."
+  echo
+  sleep 2.5
+
+fi
+
+i=0
+
+while [[ $i -le 100 ]]; do
+
+  echo -ne "\rConnecting: $i%"
+  sleep 0.06
+  ((i++))
+done
+
+echo
+echo -e "\nLinked successfully!!"
+sleep 1
+echo
+stty flush
+stty echo
